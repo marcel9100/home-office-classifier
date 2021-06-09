38b3243a56fcde07d06e41e31e6cc6ba51a29a85
marcel9100 <70887259+marcel9100@users.noreply.github.com>
2021-06-09 20:05:21 +0100

    Add files via upload

--- home_office_project.py
+++ home_office_project.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1mRe79RzyDH3GxFK8J9SodFHll-k7UNq6
 """
 
-pip install cv2
+$pip install cv2
 
 import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
